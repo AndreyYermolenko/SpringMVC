4555526c73934b46efc56736960af4cfea4c0828
AndreyYermolenko <yermolenko.a.s@gmail.com>
2020-03-02 14:42:53 +0200

    start project

---
+++ src/main/java/ua/example/controller/MainController.java
@@ -0,0 +1,14 @@
+package ua.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String view() {
+        return "index";
+    }
+
+}
