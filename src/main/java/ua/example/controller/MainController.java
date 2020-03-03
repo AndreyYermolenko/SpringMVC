9f46dd43eb6b3c4e0a262fe278c3698ee8f454bb
AndreyYermolenko <yermolenko.a.s@gmail.com>
2020-03-03 13:31:20 +0200

    added ftl

--- src/main/java/ua/example/controller/MainController.java
+++ src/main/java/ua/example/controller/MainController.java
@@ -1,14 +1,25 @@
 package ua.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String view() {
-        return "index";
+    @GetMapping("/hello")
+    public String viewHello(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name,
+                            Model model) {
+        model.addAttribute("msg", "Hello, " + name + "!");
+        return "hello";
+    }
+
+    @GetMapping("/bye")
+    public String viewBye(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name,
+                          Model model) {
+        model.addAttribute("msg", "Bye, " + name + "!");
+        return "bye";
     }
 
 }
