97cd275704093b99ef5f40b7e0884cc39b57af1c
AndreyYermolenko <yermolenko.a.s@gmail.com>
2020-03-03 23:17:06 +0200

    added spring form validation and jdbc suported

--- src/main/java/ua/example/controllers/MainController.java
+++ src/main/java/ua/example/controllers/MainController.java
@@ -1,19 +1,29 @@
 package ua.example.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ua.example.dao.UserDAO;
 import ua.example.models.User;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainController {
-    private List<User> users = new ArrayList<>();
+    //private List<User> users = new ArrayList<>();
+
+    private final UserDAO userDAO;
+
+    public MainController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 
     @GetMapping("/hello")
     public String viewHello(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name,
@@ -31,18 +41,22 @@ public class MainController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userDAO.getAll());
         return "users";
     }
 
     @GetMapping("/users/new")
-    public String getSignUp() {
+    public String getSignUp(Model model) {
+        model.addAttribute("user", new User());
         return "sign_up";
     }
 
     @PostMapping("/users/new")
-    public String signUp(@ModelAttribute User user) {
-        users.add(user);
+    public String signUp(@ModelAttribute @Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "sign_up";
+        }
+        //users.add(user);
         return "redirect:/users";
     }
 
--- src/main/java/ua/example/models/User.java
+++ src/main/java/ua/example/models/User.java
@@ -4,11 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class User {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Size(min = 1, max = 10, message = "Surname should be from 1 to 10 symbols")
     private String surname;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Incorrect email")
     private String email;
 }
